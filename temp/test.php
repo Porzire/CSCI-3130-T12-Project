de05b8456a3b69447656a533b571bba682a9ffe7
Jie Mei <mei.jie@hotmail.com>
2014-02-12 04:24:37 -0400

    Clean the repository.
    Delete demo file for jasmine test and the temp file for the project.

--- temp/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'mySQL.php';
-    $val = login('matt', 'wakaFlocka') ? 'true' : 'false';
-    echo "<p>val: ".$val."</p>";
-    $val = register('matte', 'wrong') ? 'true' : 'false';
-    echo "<p>val: ".$val."</p>";
-?>
