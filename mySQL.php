7bfe155996f167a37cce937b9744fa123da7f518
Jie Mei <mei.jie@hotmail.com>
2014-02-13 14:27:59 -0400

    Update comment for PHP file.

--- mySQL.php
+++ mySQL.php
@@ -1,79 +1,137 @@
 <?php
-    $dbusername = 'geofit';
-    $dbpass = 'charizard';
-    $dbhost = 'localhost';
-    $dbname = 'geofit';
 
-    $connection = mysql_connect($dbhost, $dbusername, $dbpass);
-    if (!$connection) {
-        die('Could not connect: ' . mysql_error());
-    }
-    $dbcheck = mysql_select_db($dbname);
-    if (!$dbcheck) {
-        die(mysql_error());
-    }
+/**
+ * This program communicates with mySQL database. It provides functions to
+ * responce mySQL queries.
+ *
+ * @version 1.0
+ */
 
-    /**
-     * Try to login with given username and password.
-     *
-     * @param String $username The user name.
-     * @param String $password The password.
-     *
-     * @return boolean True if the username and password exist in the login
-     *                 table in the database.
-     */
-    function login($username, $password) {
-        $qRes = mysql_query(
-                'SELECT password FROM login where username=\''.$username.'\'');
-        if (mysql_num_rows($qRes) > 0) {
-            $res = mysql_fetch_array($qRes);
-            if ($res[0] == $password) {
-                return true;
-            }
+/*
+ * The username we used to access mySQL database system.
+ *
+ * @var string
+ * @access private
+ */
+$dbusername = 'geofit';
+
+/*
+ * The password we used to access mySQL database system.
+ *
+ * @var string
+ * @access private
+ */
+$dbpass = 'charizard';
+
+/*
+ * The host of database we connected.
+ *
+ * @var string
+ * @access private
+ */
+$dbhost = 'localhost';
+
+/*
+ * The name of database we connected.
+ *
+ * @var string
+ * @access private
+ */
+$dbname = 'geofit';
+
+/*
+ * The connection attempt with mySQL database system.
+ *
+ * @var boolean
+ * @access private
+ */
+$connection = mysql_connect($dbhost, $dbusername, $dbpass);
+if (!$connection) {
+    die('Could not connect: ' . mysql_error());
+}
+
+/*
+ * The connection attempt with database.
+ *
+ * @var boolean
+ * @access private
+ */
+$dbcheck = mysql_select_db($dbname);
+if (!$dbcheck) {
+    die(mysql_error());
+}
+
+/*
+ * Try to login with given username and password.
+ *
+ * @param String $username The user name.
+ * @param String $password The password.
+ *
+ * @return boolean True if the username and password exist in the login
+ *                 table in the database.
+ */
+function login($username, $password) {
+    $qRes = mysql_query(
+            'SELECT password FROM login where username=\''.$username.'\'');
+    if (mysql_num_rows($qRes) > 0) {
+        $res = mysql_fetch_array($qRes);
+        if ($res[0] == $password) {
+            return true;
         }
-        return false;
     }
+    return false;
+}
 
-    /**
-     * Try to register with given username and password.
-     *
-     * @param String $username The user name.
-     * @param String $password The password.
-     *
-     * @return boolean True if the username and password have been successfully
-     *                 inserted into the login table.
-     */
-    function register($username, $password) {
-        return mysql_query(
-                'INSERT INTO login VALUES (\''.$username.'\', \''.$password.'\')');
-    }
+/*
+ * Try to register with given username and password.
+ *
+ * @param String $username The user name.
+ * @param String $password The password.
+ *
+ * @return boolean True if the username and password have been successfully
+ *                 inserted into the login table.
+ */
+function register($username, $password) {
+    return mysql_query(
+            'INSERT INTO login VALUES (\''.$username.'\', \''.$password.'\')');
+}
 
-    /**
-     * Try to remove the account with given username.
-     *
-     * @param String $username The user name.
-     *
-     * @return boolean True if the user record have been successfully removed
-     *                 from the login table.
-     */
-    function remove($username) {
-        return mysql_query(
-                'DELETE FROM login WHERE username=\''.$username.'\'');
-    }
+/*
+ * Try to remove the account with given username.
+ *
+ * @param String $username The user name.
+ *
+ * @return boolean True if the user record have been successfully removed
+ *                 from the login table.
+ */
+function remove($username) {
+    return mysql_query(
+            'DELETE FROM login WHERE username=\''.$username.'\'');
+}
 
-    $ajaxResponce = Array(
-            true => 'success',
-            false => 'fail'
-        );
+/*
+ * The connection attempt with database.
+ *
+ * @var boolean
+ * @access private
+ */
+$ajaxResponce = Array(
+        true => 'success',
+        false => 'fail'
+    );
 
-    switch ($_POST['func']) {
-        case 'login': 
-            echo $ajaxResponce[login($_POST['username'], $_POST['password'])];
-            break;
-        case 'register': 
-            echo $ajaxResponce[register($_POST['username'], $_POST['password'])];
-            break;
-        case 'remove':
-            echo $ajaxResponce[remove($_POST['username'])];
-    }
+/*
+ * This statement is used for responce ajax request.
+ */
+switch ($_POST['func']) {
+    case 'login': 
+        echo $ajaxResponce[login($_POST['username'], $_POST['password'])];
+        break;
+    case 'register': 
+        echo $ajaxResponce[register($_POST['username'], $_POST['password'])];
+        break;
+    case 'remove':
+        echo $ajaxResponce[remove($_POST['username'])];
+}
+
 ?>
